b411f061e7c49793feb8f42e745bcaaf5f49f6c0
Luchezar31 <luchezar31@abv.bg>
2026-05-16 19:05:00 +0300

    Adding CBV for delete, post, update and dashboard

--- forum/urls.py
+++ forum/urls.py
@@ -1,16 +1,15 @@
-from django.conf import settings
-from django.conf.urls.static import static
+
 from django.urls import path, include
 
 from forum import views
 
 urlpatterns = [
-    path('',views.dashboard,name='dashboard'),
+    path('',views.DashboardView.as_view(),name='dashboard'),
     path('post/',include([
-        path('add/',views.post_create_view,name='add-post'),
-        path('edit/<int:pk>',views.post_edit_form,name='edit-post'),
+        path('add/',views.PostCreateView.as_view(),name='add-post'),
+        path('edit/<int:pk>',views.EditPostView.as_view(),name='edit-post'),
         path('details/<int:pk>',views.post_details_view,name='detail-post'),
-        path('delete/<int:pk>',views.post_delete_view,name='delete_post')
+        path('delete/<int:pk>',views.DeletePostView.as_view(),name='delete_post')
     ]))
 ]
 
--- forum/views.py
+++ forum/views.py
@@ -1,64 +1,114 @@
 from django.forms.models import modelform_factory
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import UpdateView, ListView, CreateView, DeleteView, FormView
 
-from forum.forms import PostCreateForm, PostDeleteForm, SearchForm, CommentForm, CommentFormSet
+from forum.forms import PostCreateForm, PostDeleteForm, SearchForm, CommentFormSet
 from forum.models import PostBaseModel
 
 def index(request):
     return render(request,'index.html')
 
+class IndexView(View):
 
-def dashboard(request):
-    search_form = SearchForm(request.GET)
-    posts = PostBaseModel.objects.all()
-
-    if request.method == "GET" and search_form.is_valid():
-        query = search_form.cleaned_data.get('query')
-        posts = posts.filter(
-            title__icontains=query
-        )
-
-    context={
-        'posts':posts,
-        'search_form':search_form
-    }
-
-    return render(request, 'dashboard.html',context)
+    def get(self,request,*args,**kwargs):
+        return render(request,'index.html')
 
 
-def post_create_view(request):
-    form = PostCreateForm(request.POST or None,request.FILES or None)
 
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('dashboard')
+class DashboardView(ListView):
+    template_name = 'dashboard.html'
+    model = PostBaseModel
+    context_object_name = 'posts'
 
-    context = {
-        'form': form
-    }
+    def get_queryset(self):
 
-    return render(request,'create-post.html',context)
+        queryset = super().get_queryset()
+
+        self.form = SearchForm(self.request.GET)
+
+        if self.form.is_valid():
+            query = self.form.cleaned_data.get('query')
+            if query:
+                queryset = queryset.filter(title__icontains=query)
+
+        return queryset
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.form
+
+        return context
+
+# def dashboard(request):
+#     search_form = SearchForm(request.GET)
+#     posts = PostBaseModel.objects.all()
+#
+#     if request.method == "GET" and search_form.is_valid():
+#         query = search_form.cleaned_data.get('query')
+#         posts = posts.filter(
+#             title__icontains=query
+#         )
+#
+#     context={
+#         'posts':posts,
+#         'search_form':search_form
+#     }
+#
+#     return render(request, 'dashboard.html',context)
 
 
-def post_edit_form(request,pk):
-    post = PostBaseModel.objects.get(pk=pk)
+class PostCreateView(CreateView):
+    form_class = PostCreateForm
+    template_name = 'create-post.html'
+    success_url = reverse_lazy('dashboard')
 
-    if request.user.is_superuser:
-        PostEditForm = modelform_factory(PostBaseModel, fields='__all__')
-    else:
-        PostEditForm = modelform_factory(PostBaseModel, fields=('context',))
+#
+# def post_create_view(request):
+#     form = PostCreateForm(request.POST or None,request.FILES or None)
+#
+#     if request.method == 'POST' and form.is_valid():
+#         form.save()
+#         return redirect('dashboard')
+#
+#     context = {
+#         'form': form
+#     }
+#
+#     return render(request,'create-post.html',context)
 
-    form = PostEditForm(request.POST or None, instance=post )
 
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('dashboard')
+class EditPostView(UpdateView):
+    model = PostBaseModel
+    template_name = 'edit-post.html'
+    success_url = reverse_lazy('dashboard')
 
-    context = {
-        'form':form
-    }
+    def get_form_class(self):
+        if self.request.user.is_authenticated:
+            return modelform_factory(PostBaseModel,fields='__all__')
+        return modelform_factory(PostBaseModel,fields=('context',))
 
-    return render(request,'edit-post.html',context)
+
+# def post_edit_form(request,pk):
+#     post = PostBaseModel.objects.get(pk=pk)
+#
+#     if request.user.is_superuser:
+#         PostEditForm = modelform_factory(PostBaseModel, fields='__all__')
+#     else:
+#         PostEditForm = modelform_factory(PostBaseModel, fields=('context',))
+#
+#     form = PostEditForm(request.POST or None, instance=post )
+#
+#     if request.method == 'POST' and form.is_valid():
+#         form.save()
+#         return redirect('dashboard')
+#
+#     context = {
+#         'form':form
+#     }
+#
+#     return render(request,'edit-post.html',context)
 
 def post_details_view(request, pk):
     post = PostBaseModel.objects.get(pk=pk)
@@ -81,6 +131,17 @@ def post_details_view(request, pk):
 
     return render(request,'post-details.html',context)
 
+
+class DeletePostView(DeleteView,FormView):
+    form_class = PostDeleteForm
+    model = PostBaseModel
+    success_url = reverse_lazy('dashboard')
+    template_name = 'delete-post.html'
+
+    def get_initial(self):
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        post = PostBaseModel.objects.get(pk=pk)
+        return post.__dict__
 
 def post_delete_view(request,pk):
     post = PostBaseModel.objects.get(pk=pk)
