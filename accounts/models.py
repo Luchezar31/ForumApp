ee10fdf5d83931f5f3dcf5d6af39cedbb6d60ae5
Lachezar Yordanov <lachezar.yordanov@flatrocktech.com>
2026-09-11 10:57:30 +0300

    some changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
-=======
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
->>>>>>> 3e503cac61284b7e60aa02e3e329fdd2b1da61fe
 from django.db import models
 from django.db.models import OneToOneField
 
@@ -11,49 +7,16 @@ from accounts.managers import AppUserManager
 
 from accounts.managers import CustomUserManager
 
-<<<<<<< HEAD
-=======
 # class CustomUser(AbstractUser):
 #     points = models.IntegerField(
 #         blank=True,
 #         null=True
 #     )
 #
->>>>>>> 3e503cac61284b7e60aa02e3e329fdd2b1da61fe
 
 class AppCustomUser(AbstractBaseUser, PermissionsMixin):
 
     username = models.CharField(
-<<<<<<< HEAD
-        max_length=100,
-        unique=True,
-        error_messages={
-            'unique':"User with that name already exist"
-        }
-    )
-
-    email = models.EmailField(
-        unique=True,
-
-    )
-
-    is_staff = models.BooleanField(
-        default=False
-    )
-
-    is_active = models.BooleanField(
-        default=True
-    )
-
-    date_joined = models.DateField(
-        auto_now_add=True
-    )
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username',]
-
-    objects = CustomUserManager()
-=======
         unique=True,
         max_length=150
     )
@@ -114,4 +77,3 @@ class Profile(models.Model):
 
 
 
->>>>>>> 3e503cac61284b7e60aa02e3e329fdd2b1da61fe
